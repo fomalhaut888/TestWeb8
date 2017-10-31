5afcb406ab4338c208104109730fa57ae8b41c1c
Simon Chen <SimonC@leosys.com>
2017-10-31 16:45:26 +0800

    Binary Literals & Underscores in Number Literals

---
+++ src/new7/BinaryLiteral.java
@@ -0,0 +1,71 @@
+package new7;
+
+/**
+ * Samples for Binary Literals
+ * @author SimonC
+ *
+ */
+public class BinaryLiteral {
+
+	public static void main(String[] args) {
+			// An 8-bit 'byte' value:
+			byte aByte1 = (byte)0b00100001;//2^5 + 1 = 33
+			System.out.println("aByte1=" + aByte1);
+			// An 16-bit value to a byte integral
+			byte aByte2 = (byte)0b0010000100100001;
+			System.out.println("aByte2=" + aByte2);
+			
+			// A 16-bit 'short' value:
+			//With 2's complement, 
+			// 0b1010000101000101 - 0b0000000000000001 = 0b1010000101000100 
+			// inversed as 0b0101111010111011 = 24521, 
+			// so the decimal of this binary value is -24521.
+			short aShort1 = (short)0b1010000101000101;
+			System.out.println("aShort1=" + aShort1);
+			// An 32-bit value to a short integral
+			short aShort2 = (short)0b10100001010001011010000101000101;
+			System.out.println("aShort2=" + aShort2);
+			
+			// Some 32-bit 'int' values:
+			int anInt1 = 0b10100001010001011010000101000101;
+			System.out.println("anInt1=" + anInt1);
+			int anInt2 = 0b101;//2^2 + 1
+			System.out.println("anInt2=" + anInt2);
+			int anInt3 = 0B101; // The B can be upper or lower case.
+			System.out.println("anInt3=" + anInt3);
+			
+			// A 64-bit 'long' value. Note the "L" suffix:
+			// Without L, a compile error for the literal of type int which is out of range;
+			long aLong = 0b1010000101000101101000010100010110100001010001011010000101000101L;
+			System.out.println("aLong=" + aLong);
+			
+			//Binary literals can make relationships among data more apparent 
+			//  than they would be in hexadecimal or octal. 
+			//For example, each successive number in the following array is rotated by one bit:
+			int[] phases1 = {
+					0b00110001,
+					0b01100010,
+					0b11000100,
+					0b10001001,
+					0b00010011,
+					0b00100110,
+					0b01001100,
+					0b10011000
+			};
+			System.out.print("phases1: ");
+			for(int i: phases1){
+					System.out.print(i + ",");
+			}
+			System.out.println();
+			//In hexadecimal, the relationship among the numbers is not readily apparent:
+			int[] phases2 = {
+					0x31, 0x62, 0xC4, 0x89, 0x13, 0x26, 0x4C, 0x98
+			};
+			System.out.print("phases2: ");
+			for(int i: phases2){
+					System.out.print(i + ",");
+			}
+			System.out.println();
+	}
+
+}
---
+++ src/new7/UnderscoresInNumberLiterals.java
@@ -0,0 +1,59 @@
+package new7;
+
+/**
+ * Samples for Underscores in Numeric Literals
+ * @author SimonC
+ *
+ */
+public class UnderscoresInNumberLiterals {
+
+	public static void main(String[] args) {
+			//Any number of underscore characters (_) can appear anywhere between digits in a numerical literal.
+			long creditCardNumber = 1234_5678_9012_3456L;
+			System.out.println("creditCardNumber=" + creditCardNumber);
+			long socialSecurityNumber = 999_99_9999L;
+			System.out.println("socialSecurityNumber=" + socialSecurityNumber);
+			long adwordsCid = 123_456_7890L;
+			System.out.println("adwordsCid=" + adwordsCid);
+			float pi = 3.14_15F;
+			System.out.println("pi=" + pi);
+			long hexBytes = 0xFF_EC_DE_5E;
+			System.out.println("hexBytes=" + hexBytes);
+			long hexWords = 0xCAFE_BABE;
+			System.out.println("hexWords=" + hexWords);
+			long maxLong = 0x7fff_ffff_ffff_ffffL;
+			System.out.println("maxLong=" + maxLong);
+			byte nybbles = 0b0010_0101;
+			System.out.println("nybbles=" + nybbles);
+			long bytes = 0b11010010_01101001_10010100_10010010;
+			System.out.println("bytes=" + bytes);
+			
+			//You can place underscores only between digits; you cannot place underscores in the following places:
+			//	- At the beginning or end of a number
+			//	- Adjacent to a decimal point in a floating point literal
+			//  - Prior to an F or L suffix
+			//  - In positions where a string of digits is expected
+			//A Compile error 'Underscores have to be located within digits'
+			//float pi1 = 3_.1415F;      // Invalid; cannot put underscores adjacent to a decimal point
+			//float pi2 = 3._1415F;      // Invalid; cannot put underscores adjacent to a decimal point
+			//long socialSecurityNumber1 = 999_99_9999_L;// Invalid; cannot put underscores prior to an L suffix
+			//A Compile error '_52 cannot be resolved to a variable'
+			//int x1 = _52;// This is an identifier, not a numeric literal
+			int x2 = 5_2;// OK (decimal literal)
+			System.out.println("x2=" + x2);
+			//int x3 = 52_;// Invalid; cannot put underscores at the end of a literal
+			int x4 = 5_______2;// OK (decimal literal)
+			System.out.println("x4=" + x4);
+			//int x5 = 0_x52;// Invalid; cannot put underscores in the 0x radix prefix
+			//int x6 = 0x_52;// Invalid; cannot put underscores at the beginning of a number
+			int x7 = 0x5_2;// OK (hexadecimal literal)
+			System.out.println("x7=" + x7);
+			//int x8 = 0x52_;// Invalid; cannot put underscores at the end of a number
+			int x9 = 0_52;// OK (octal literal)
+			System.out.println("x9=" + x9);
+			int x10 = 05_2;// OK (octal literal)
+			System.out.println("x10=" + x10);
+			//int x11 = 052_;// Invalid; cannot put underscores at the end of a number
+	}
+
+}
