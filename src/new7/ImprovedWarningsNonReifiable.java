007e0cad2c3bf055fb873a18459920518a418c92
Simon Chen <SimonC@leosys.com>
2017-11-03 15:01:02 +0800

    Non-Reifiable formal parameters

--- src/new7/ImprovedWarningsNonReifiable.java
+++ src/new7/ImprovedWarningsNonReifiable.java
@@ -1,6 +1,7 @@
 package new7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ImprovedWarningsNonReifiable {
@@ -24,11 +25,69 @@ public class ImprovedWarningsNonReifiable {
 			//Because of type erasure, the type of the second formal parameter 
 			//   of the add method (which is defined as List<E>.add(int,E)) becomes Object.
 			l.add(0, new Integer(42)); // another unchecked warning
-			String s = ls.get(0);      // ClassCastException is thrown
-			System.out.println("s=" + s);
+			//String s = ls.get(0);      // ClassCastException is thrown
+			//System.out.println("s=" + s);
 			
 			//2. Variable Arguments Methods and Non-Reifiable Formal Parameters
-			//TODO
+			//Potential heap pollution may happen when using varargs with non-reifiable formal parameters.
+			//The Java SE 7 generates the warning at both the declaration site and the call site.
+			//The Java SE 5 and 6 compilers only generate this warning at the call site but not at the declaration site.
+			List<String> stringListA = new ArrayList<String>();
+		    List<String> stringListB = new ArrayList<String>();
+		    
+		    addToList(stringListA, "Seven", "Eight", "Nine");
+		    addToList(stringListA, "Ten", "Eleven", "Twelve");
+		    System.out.println("stringListA=" + stringListA);
+		    
+		    List<List<String>> listOfStringLists = new ArrayList<List<String>>();
+		    //Compile warning: Type safety: A generic array of List<String> is created for a varargs parameter
+		    addToList(listOfStringLists, stringListA, stringListB);
+		    System.out.println("listOfStringLists=" + listOfStringLists);
+		    
+		    //3. Potential Vulnerabilities of Varargs Methods with Non-Reifiable Formal Parameters
+		    //Compile warning: Type safety: A generic array of List<String> is created for a varargs parameter
+		    faultyMethod(Arrays.asList("Hello!"), Arrays.asList("World!"));
+		    
+		    //4. Suppressing Warnings from Varargs Methods with Non-Reifiable Formal Parameters
+		    //Compile warning yet when the method with @SuppressWarnings("unchecked") is called: 
+		    //  Type safety: A generic array of List<String> is created for a varargs parameter
+		    addToList2(listOfStringLists, stringListA, stringListB);
+		    System.out.println("listOfStringLists=" + listOfStringLists);
+		    //No warnings are generated either at the method's declaration or when it is called.
+		    //  This method uses @SafeVarargs.
+		    addToList3(listOfStringLists, stringListA, stringListB);
+		    System.out.println("listOfStringLists=" + listOfStringLists);
+		    //Use the compiler option -Xlint:varargs.
+		    //TODO when you are idle.
 	}
 
+	//Compile warning: Type safety: Potential heap pollution via varargs parameter since JDK7
+	public static <T> void addToList (List<T> listArg, T... elements) {
+			for (T x : elements) {
+					listArg.add(x);
+			}
+	}
+	
+	@SuppressWarnings("unchecked")
+	//@SuppressWarnings({"unchecked", "varargs"}) //Unsupported @SuppressWarnings("varargs")
+	public static <T> void addToList2 (List<T> listArg, T... elements) {
+			for (T x : elements) {
+					listArg.add(x);
+			}
+	}
+	
+	@SafeVarargs
+	public static <T> void addToList3 (List<T> listArg, T... elements) {
+			for (T x : elements) {
+					listArg.add(x);
+			}
+	}
+	
+	//Compile warning: Type safety: Potential heap pollution via varargs parameter l since JDK7
+	public static void faultyMethod(List<String>... l) {
+			//This statement can potentially introduce heap pollution.
+			Object[] objectArray = l;  // Valid
+			objectArray[0] = Arrays.asList(new Integer(42));
+			//String s = l[0].get(0);    // ClassCastException thrown here
+	}
 }
